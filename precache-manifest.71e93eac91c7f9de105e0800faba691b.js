4e33f652ae2f9b7c482508d25733d4f76c114934
Danil <conovalov.danil3441@gmail.com>
2020-03-27 02:38:27 +0400

    Updates

---
+++ precache-manifest.71e93eac91c7f9de105e0800faba691b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827469fdc68c099561714c0aad33a4d4",
+    "url": "/danil4905/-Terminal-/index.html"
+  },
+  {
+    "revision": "02de9241b9ff9eeb7491",
+    "url": "/danil4905/-Terminal-/static/css/main.f69e50da.chunk.css"
+  },
+  {
+    "revision": "f6dbeea41230daf9c93c",
+    "url": "/danil4905/-Terminal-/static/js/2.cbe9a7a6.chunk.js"
+  },
+  {
+    "revision": "293f831819028c82d84f70278e51e4f7",
+    "url": "/danil4905/-Terminal-/static/js/2.cbe9a7a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02de9241b9ff9eeb7491",
+    "url": "/danil4905/-Terminal-/static/js/main.2cb759b4.chunk.js"
+  },
+  {
+    "revision": "f3db097da43ee0299469",
+    "url": "/danil4905/-Terminal-/static/js/runtime-main.98b987b3.js"
+  }
+]);
--- src/Redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import Reducer from "./reducer";
-
-let store = createStore(Reducer);
-window.store = store;
-export default store;
